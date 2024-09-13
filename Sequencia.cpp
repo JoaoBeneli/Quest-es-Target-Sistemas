3b86c9a6cf9029937933d179037607a5a535f87e
João Beneli <133045596+JoaoBeneli@users.noreply.github.com>
2024-09-12 22:04:52 -0300

    Add files via upload

---
+++ Sequencia.cpp
@@ -0,0 +1,22 @@
+/** 
+A lógica das questões: 
+
+a) Sequencia de números impares:
+1,3,5,7,[9]
+
+b) Cada número é o dobro do número anterior:
+2, 4, 8, 16, 32, 64,[128]
+
+c) Sequencia de quadrados :
+0, 1, 4, 9, 16, 25, 36, [49] 
+
+d) Sequencia de quadrados somente dos números pares:
+4, 16, 36, 64, [100]
+
+e) Cada número é a soma dos dois números anteriores (Fibonacci):
+ 1, 1, 2, 3, 5, 8, [13]
+
+ f) (2+8=10) / (10+2=12) / (12+4=16) / (16+1=17) / (17+1=18) / (18+1=19) / (19+1=20)
+ 2, 10, 12, 16, 17, 18, 19, [20] 
+
+ **/

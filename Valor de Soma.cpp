44e75826e694c0b0ee72270a96bc94b750093dfa
João Beneli <133045596+JoaoBeneli@users.noreply.github.com>
2024-09-12 21:45:13 -0300

    Add files via upload

---
+++ Valor de Soma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int indice = 12, soma = 0, k = 1;
+
+
+    while (k < indice) {
+        k = k + 1;
+        soma = soma + k;
+    }
+
+
+    cout << "O valor de soma é: " << soma << endl;
+
+    return 0;
+}

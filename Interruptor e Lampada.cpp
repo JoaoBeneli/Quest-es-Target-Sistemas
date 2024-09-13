318ad7d2a2c6fe7a8955a5e84e143a64f22c1a1d
João Beneli <133045596+JoaoBeneli@users.noreply.github.com>
2024-09-12 22:16:16 -0300

    Add files via upload

---
+++ Interruptor e Lampada.cpp
@@ -0,0 +1,11 @@
+/**
+Resposta:
+
+ Ligaria um interruptor e deixaria um tempo ligado, fazendo com que a lâmpada se aqueça,
+ após algum tempo desligaria esse interruptor e ligaria o próximo e deixaria acesso e 
+ checaria as salas, a sala com a lâmpada quente eu saberia que foi do primeiro interruptor,
+ caso a lâmpada esteja acessa, ela seria do segundo interruptor, e caso esteja fria seria do
+ terceiro interruptor.
+
+
+ **/
